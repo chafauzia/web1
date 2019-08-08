9061a0e96370d425dec715ddec037581aa09f98b
Anisa Fauzia <43779928+chafauzia@users.noreply.github.com>
2019-08-08 18:04:17 +0700

    Create tambah-pelanggan.php

---
+++ tambah-pelanggan.php
@@ -0,0 +1,10 @@
+<form method=POST action=daftar-aksi.php>
+<table>
+<tr><td>Username</td>     <td> : <input type=text name='username'></td></tr>
+<tr><td>Password</td>     <td> : <input type=password name='password'></td></tr>
+<tr><td>Nama Lengkap</td> <td> : <input type=text name='nama_lengkap' size=30></td></tr>  
+<tr><td valign=top>Alamat</td><td> &nbsp; <textarea name=alamat  style='width: 315px; height: 100px;'></textarea></td><tr>
+<tr><td>No.Telp/HP</td>   <td> : <input type=text name='no_telp' size=20></td></tr>
+<tr><td>E-mail</td>       <td> : <input type=text name='email' size=30></td></tr>
+<tr><td colspan=2><input type=submit value=Simpan><input type=reset value=Batal></td></tr>
+</table></form>
