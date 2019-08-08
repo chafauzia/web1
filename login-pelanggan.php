0187be52b7501db8c1bc194f9d668504f0ab2dd6
Anisa Fauzia <43779928+chafauzia@users.noreply.github.com>
2019-08-08 18:05:10 +0700

    Create login-pelanggan.php

---
+++ login-pelanggan.php
@@ -0,0 +1,42 @@
+
+
+<form action="cek-login.php" method="post">
+	<table width="100%" border="0" align="center" cellpadding="0" cellspacing="0">
+	  <tr>
+	    <td>&nbsp;</td>
+	    <td>&nbsp;</td>
+	    <td>&nbsp;</td>
+	    <td>&nbsp;</td>
+      </tr>
+      <tr>
+	    <td height="31">&nbsp;</td>
+	    <td colspan="3"><strong>LOGIN PELANGGAN</strong></td>
+      </tr>
+      <tr>
+        <td width="48">&nbsp;</td>
+        <td width="198" class="style2">Username</td>
+        <td width="13">&nbsp;</td>
+        <td width="317"><label>
+          <input name="username" type="text" size="15" />
+        </label></td>
+      </tr>
+      <tr>
+        <td width="48">&nbsp;</td>
+        <td class="style2">Password</td>
+        <td></td>
+        <td><input name="pass" type="password" size="15" /></td>
+      </tr>
+      <tr>
+        <td width="48">&nbsp;</td>
+        <td>&nbsp;</td>
+        <td></td>
+        <td><input type="submit" name="Submit" value="LOGIN" /></td>
+      </tr>
+      <tr>
+        <td colspan="4"><div align="left">
+          <ul>
+            <li><a href="?page=tambah-pelanggan">Daftar Baru </a></li>
+          </ul>
+        </div></td>
+      </tr>
+</table></form>
