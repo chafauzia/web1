2d3a4fdaf1cb0ed58b1d3839f11ea517ac870bda
Anisa Fauzia <43779928+chafauzia@users.noreply.github.com>
2019-08-08 18:03:12 +0700

    Create home.php

---
+++ home.php
@@ -0,0 +1,7 @@
+<p><br />
+  <b>Assalamu'alaikum</b><br />
+  <br />
+
+Selamat Datang di website Adventure. </p>
+<p><img src="gambar/New%20folder/images/sleeping%20bag/sleeping-bag-suit%20consinia%20160.jpg" width="531" height="576"></p>
+<p>&nbsp;</p>
